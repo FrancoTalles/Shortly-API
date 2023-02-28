778f78de0680d02b2ffdba49c6ffc676b90f325e
FrancoTalles <francotallesextreme@gmail.com>
2023-02-28 17:40:37 -0400

    feat urls/:id

--- src/Controllers/Urls_Controllers.js
+++ src/Controllers/Urls_Controllers.js
@@ -12,15 +12,43 @@ export async function encurtaLinks(req, res) {
       [shortUrl, url, user_id]
     );
 
-    const resgatando_o_id = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1;`, [shortUrl]);
+    const resgatando_o_id = await db.query(
+      `SELECT * FROM urls WHERE "shortUrl" = $1;`,
+      [shortUrl]
+    );
     const id_pego = resgatando_o_id.rows[0].id;
 
     const resBody = {
       id: id_pego,
-      shortUrl: shortUrl
+      shortUrl: shortUrl,
     };
 
     res.status(201).send(resBody);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function pegaLinkPeloId(req, res) {
+  const { id } = req.params;
+
+  try {
+
+    const procura_url = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
+  
+    if (procura_url.rowCount === 0){
+      return res.sendStatus(404);
+    }
+
+    const url = procura_url.rows[0];
+
+    const resBody = {
+      id: url.id,
+      shortUrl: url.shortUrl,
+      url: url.url
+    }
+
+    res.status(200).send(resBody);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- src/Routers/Urls_Router.js
+++ src/Routers/Urls_Router.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
-import { encurtaLinks } from "../Controllers/Urls_Controllers.js";
+import {
+  encurtaLinks,
+  pegaLinkPeloId,
+} from "../Controllers/Urls_Controllers.js";
 import { tokenValidation } from "../Middlewares/Urls_Middlewares.js";
 import { validateSchema } from "../Middlewares/validateSchema.js";
 import { url_schema } from "../Schemas/Url_Schema.js";
 
 const UrlRouter = Router();
 
-UrlRouter.post("/urls/shorten", validateSchema(url_schema), tokenValidation, encurtaLinks);
+UrlRouter.post(
+  "/urls/shorten",
+  validateSchema(url_schema),
+  tokenValidation,
+  encurtaLinks
+);
+UrlRouter.get("/urls/:id", pegaLinkPeloId);
 
 export default UrlRouter;
